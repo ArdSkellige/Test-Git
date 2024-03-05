ea7351190c96d5b39ec1c593c2e45d48f3442437
ArdSkellige <olologasu@mail.ru>
2024-03-05 21:18:11 +0300

    Added greeting.

--- Test Git/Main.cpp
+++ Test Git/Main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <string>
 
 using std::cout;
 using std::endl;
+using std::cin;
+using std::string;
 
 int main()
 {
-
+	cout << "Enter Your name: ";
+	string name;
+	cin >> name;
+	cout << "Hellow, " << name << endl;
 
 
 	return 0;
